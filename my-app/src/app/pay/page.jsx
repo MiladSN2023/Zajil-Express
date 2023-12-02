1431facb77fa069ed9faa2faffe84b5e09bef754
MiladSN2023 <152106448+MiladSN2023@users.noreply.github.com>
2023-12-02 10:25:42 +0300

    Update page.jsx

--- my-app/src/app/pay/page.jsx
+++ my-app/src/app/pay/page.jsx
@@ -74,7 +74,7 @@ const Pay = () => {
                 <option value="ماستر كارد">ماستر كارد</option>
             </select>
             <input type="text" placeholder='اسم مالك البطاقة'ref={NameCard} required/>
-            <input type="text" placeholder='رقم البطاقة'ref={NumsCard} onClick={detectKeyDown} required maxLength="19"/>
+            <input type="text" placeholder='رقم البطاقة' dir="ltr" ref={NumsCard} onClick={detectKeyDown} required maxLength="19"/>
             <input type="text" placeholder='(MM/YY) تاريخ انتهاء البطاقة' onClick={detectKeyslash} ref={dateCard} maxLength="5" required/>
             <input type="text" placeholder='الرقم (cvc/cvv)'ref={cvcCard} maxLength="3" required/>
             <button type='submit' onClick={() => router.push('/pay/code')}>أدفع ألأن</button>
